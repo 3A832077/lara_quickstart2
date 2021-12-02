5be5dd8f6af48ecfd38d6ccb1265d1f2ba524f63
chen shu ting <a.0066715@gmail.com>
2021-12-02 20:18:44 +0800

    新增關聯

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+	protected $fillable = ['name'];
+	
+	$user = App\User::find(1);
+    foreach ($user->tasks as $task) {
+    echo $task->name;
+}
+}
